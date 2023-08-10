d07386285201843075e5891f9fac325a6822f1ef
Ramanuj <75874452+Ramanuj-1729@users.noreply.github.com>
2023-08-10 11:50:30 +0530

    add selectedPlan page

--- frontend/src/pages/SelectedPlan/SelectedPlan.jsx
+++ frontend/src/pages/SelectedPlan/SelectedPlan.jsx
@@ -1,8 +1,28 @@
-
+import styles from './SelectedPlan.module.css';
 
 const SelectedPlan = () => {
     return (
-        <div>SelectedPlan</div>
+        <div className={`${styles.planScreen} flex-center`}>
+            <div className={`${styles.cardWrapper}`}>
+                <div className={styles.headingWrapper}>
+                    <h4>Current Plan Details</h4>
+                    <span>Active</span>
+                </div>
+
+                <button className={styles.cancleBtn}>Cancel</button>
+
+                <span className={styles.plan}>Basic</span>
+                <span className={styles.device}>Phone+Tablet</span>
+
+                <div className={styles.price}>₹ 2,000<span>/yr</span></div>
+
+                <button className={styles.mainBtn}>Change Plan</button>
+
+                <div className={styles.warn}>
+                    Your subscription has started on <span>Jul 11th, 2022</span> and will auto renew on <span>Jul 12th, 2023</span>.
+                </div>
+            </div>
+        </div>
     );
 }
 
